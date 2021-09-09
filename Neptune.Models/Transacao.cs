8b84e91a030aea1c63d1a5c4969cb67a7aefbca4
Jenkins <alm.jenkins@viavarejo.com.br>
2021-09-09 10:00:02 -0300

    Limpeza e Inputs Blazor

--- Neptune.Models/Transacao.cs
+++ Neptune.Models/Transacao.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Neptune.Models
 {
-    public class Transacao 
+    public class Transacao
     {
-        public byte IdView { get; set; }
-        public DateTime Data { get; set; } = DateTime.Now;
-        public string Descricao { get; set; }
-        public decimal Valor { get; set; }
         public int Id { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Descricao { get; set; }
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        public byte IdView { get; set; }
 
         public Transacao()
         {
